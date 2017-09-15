ba72fdba089f49d0b31279f54351f5f29673689a
吴昭琪 <1944169531@qq.com>
2017-09-15 18:04:20 +0800

    uodate:头部，菜单三等分

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
   </div>
 </template>
 
 <script>
+  import header from './components/header/header.vue';
   export default {
-    name: 'app',
-    components: ['header']
+    components: {
+      'v-header': header
+    }
   };
 </script>
 
@@ -19,6 +25,16 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    margin-top: 10px;
+  }
+  .tab{
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height:40px;
+  }
+  .tab-item{
+    flex:1;
+    text-align: center;
   }
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -6,9 +6,17 @@
 </template>
 
 <script type="text/ecmascript-6">
-
+  module.exports = {
+    data: function () {
+      return {
+        title: 'this is a todo list'
+      };
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .header {
+    height: 20px
+  }
 </style>
