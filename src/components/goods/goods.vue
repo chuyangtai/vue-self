e94685a48fa93117960249e5eb90b320107c85f6
吴昭琪 <1944169531@qq.com>
2017-12-12 18:20:59 +0800

    update:购物车详情展开

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,82 +1,82 @@
 <template>
-    <div class="goods">
-      <div class="menu-wrapper">
-        <ul>
-          <li v-for="item in goods" class="menu-item"><span>{{item.name}}</span></li>
-        </ul>
-      </div>
-      <div class="foods-wrapper">
-        <ul>
-          <li v-for="item in goods" class="food-list">
-            <h1 class="title">{{item.name}}</h1>
-            <ul>
-              <li v-for="food in item.foods" class="food-item">
-                <div class="icon">
-                  <img :src="food.icon" alt="" width="57">
-                </div>
-                <div class="content">
-                  <h2 class="name">{{food.name}}</h2>
-                  <div class="extra">
-                    <span class="count">月售{{food.sellCount}}</span>
-                    <span class="rating">好评率{{food.rating}}%</span>
-                  </div>
-                  <div class="price">
-                    <span class="now">￥{{food.price}}</span>
-                    <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
-                  </div>
-                  <div class="cart-control-wrapper">
-                    <cartControl :food="food" @addElem="_drop"></cartControl>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <shop-cart ref="shopCart" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice" :selectFoods="selectFoods"></shop-cart>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item"><span>{{item.name}}</span></li>
+      </ul>
     </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img :src="food.icon" alt="" width="57">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}</span>
+                  <span class="rating">好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
+                </div>
+                <div class="cart-control-wrapper">
+                  <cartControl :food="food" @addElem="_drop"></cartControl>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <shop-cart ref="shopCart" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"
+               :selectFoods="selectFoods"></shop-cart>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-import shopCart from '../shopcart/shopcart.vue';
-import cartControl from '../cartcontrol/cartcontrol.vue';
+  import shopCart from '../shopcart/shopcart.vue';
+  import cartControl from '../cartcontrol/cartcontrol.vue';
 
-const ERR_OK = 0;
+  const ERR_OK = 0;
 
-export default{
-  props: {
-    seller: {
-    }
-  },
-  components: {
-    shopCart,
-    cartControl
-  },
-  methods: {
+  export default{
+    props: {
+      seller: {}
+    },
+    components: {
+      shopCart,
+      cartControl
+    },
+    methods: {
       // 调用 子组件shopCart的方法
-    _drop (el) {
+      _drop (el) {
         // 异步执行动画，优化下落动画体验
         this.$nextTick(() => {
           this.$refs.shopCart.drop(el);
         });
-    }
-  },
-  data () {
-    return {
-      goods: {}
-    };
-  },
-  created () {
-    this.axios.get('/api/goods').then((response) => {
-      if (response.data.errno === ERR_OK) {
-        this.goods = response.data.data;
       }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  },
-  computed: {
+    },
+    data () {
+      return {
+        goods: {}
+      };
+    },
+    created () {
+      this.axios.get('/api/goods').then((response) => {
+        if (response.data.errno === ERR_OK) {
+          this.goods = response.data.data;
+        }
+      })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    computed: {
       selectFoods () {
         let foods = [];
         this.goods.forEach((good) => {
@@ -88,33 +88,37 @@ export default{
         });
         return foods;
       }
-  }
-};
+    }
+  };
 </script>
 
 <style>
-.cart-control-wrapper{
-  position:absolute;
-  right: 0;
-  bottom:2px;
-}
-.goods{
-  display: flex;
-  position: absolute;
-  top:180px;
-  width:100%;
-  bottom: 46px;
-  overflow: hidden;
-}
-  .menu-wrapper{
-    flex:0 0 80px;
+  .cart-control-wrapper {
+    position: absolute;
+    right: 0;
+    bottom: 2px;
+  }
+
+  .goods {
+    display: flex;
+    position: absolute;
+    top: 180px;
+    width: 100%;
+    bottom: 46px;
+    overflow: hidden;
+  }
+
+  .menu-wrapper {
+    flex: 0 0 80px;
     overflow: scroll;
   }
-.menu-wrapper ul{
-  padding: 0 8px;
-  background: #f3f5f7;
-}
-  .menu-item{
+
+  .menu-wrapper ul {
+    padding: 0 8px;
+    background: #f3f5f7;
+  }
+
+  .menu-item {
     height: 54px;
     border-bottom: 1px solid #d2d3d6;
     text-align: center;
@@ -123,17 +127,20 @@ export default{
     font-size: 12px;
     width: 56px;
   }
-.menu-item span{
-  display: table-cell;
-  vertical-align: middle;
-}
-  .foods-wrapper{
-    flex:1;
+
+  .menu-item span {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .foods-wrapper {
+    flex: 1;
     overflow: scroll;
   }
-.foods-wrapper .title{
+
+  .foods-wrapper .title {
     font-size: 12px;
-    color: rgb(147,153,159);
+    color: rgb(147, 153, 159);
     padding-left: 14px;
     line-height: 26px;
     border-left: 2px solid #d9dde1;
@@ -141,58 +148,69 @@ export default{
     margin: 0;
     width: 100%;
   }
-/*每一个food-item是一个flex布局*/
-.food-item{
-  display: flex;
-  margin: 18px;
-  padding-bottom: 18px;
-  border-bottom: 1px solid #ececec;
-}
-.food-item:last-child{
-  margin-bottom: 0;
-}
-.icon{
-  flex:0 0 57;
-  margin-right: 10px;
-}
-.content{
-   flex:1;
-  position: relative;
+
+  /*每一个food-item是一个flex布局*/
+  .food-item {
+    display: flex;
+    margin: 18px;
+    padding-bottom: 18px;
+    border-bottom: 1px solid #ececec;
   }
-.content .name{
-  margin: 2px 0 8px 0;
-  line-height:14px;
-  font-size: 14px;
-  color: rgb(7,17,27);
-}
-.content .description{
-  margin-bottom:8px;
-  font-size: 10px;
-  line-height: 10px;
-  color: rgba(147,153,159,0.8);
-}
-.extra{
-  line-height:10px;
-  font-size: 10px;
-  color: #878787;
-  margin-bottom:8px;
-}
-.extra .count{
-  margin-right:8px;
-}
-  .price{
-    font-weight:700;
-    line-height:24px;
+
+  .food-item:last-child {
+    margin-bottom: 0;
   }
-  .now{
-    margin-right:8px;
+
+  .icon {
+    flex: 0 0 57;
+    margin-right: 10px;
+  }
+
+  .content {
+    flex: 1;
+    position: relative;
+  }
+
+  .content .name {
+    margin: 2px 0 8px 0;
+    line-height: 14px;
     font-size: 14px;
-    color: rgb(240,20,20);
+    color: rgb(7, 17, 27);
   }
-  .old{
+
+  .content .description {
+    margin-bottom: 8px;
+    font-size: 10px;
+    line-height: 10px;
+    color: rgba(147, 153, 159, 0.8);
+  }
+
+  .extra {
+    line-height: 10px;
+    font-size: 10px;
+    color: #878787;
+    margin-bottom: 8px;
+  }
+
+  .extra .count {
+    margin-right: 8px;
+  }
+
+  .price {
+    font-weight: 700;
+    line-height: 24px;
+  }
+
+  .now {
+    margin-right: 8px;
+    font-size: 14px;
+    color: rgb(240, 20, 20);
+  }
+
+  .old {
     text-decoration: line-through;
     font-size: 10px;
-    color:rgb(147,153,159);
+    color: rgb(147, 153, 159);
   }
 
 </style>
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="shopcart">
+  <div class="shopcart" @click="listShow=!listShow">
     <div class="content">
       <div class="content-left">
         <div class="logo" :class="{'highlight':totleCount > 0}">
@@ -14,6 +14,7 @@
         </div>
       </div>
     </div>
+    <!--小球掉落-->
     <div class="ball-container">
       <div v-for="ball in balls">
         <transition name="dropBall" @before-enter="beforeEnter" @enter="enter" @afterEnter="afterEnter">
@@ -23,28 +24,50 @@
         </transition>
       </div>
     </div>
-	</div>
-
+    <!--购物车展开详情-->
+    <transition name="cartFold">
+      <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty">清空</span>
+          </div>
+          <div class="list-content">
+            <ul>
+              <li class="food" v-for="food in selectFoods">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>{{food.price * food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import cartControl from '../cartcontrol/cartcontrol.vue';
   export default{
     props: {
-        deliveryPrice: {
-          default: 0
-        },
-        minPrice: {
-          default: 0
-        },
+      deliveryPrice: {
+        default: 0
+      },
+      minPrice: {
+        default: 0
+      },
       selectFoods: {
         type: Array,
         default () {
-            return [
-              {price: 0, count: 0}
-            ];
+          return [
+            {price: 0, count: 0}
+          ];
         }
-       }
-      },
+      }
+    },
     methods: {
       drop (el) {
         // 可以得到这个点击元素
@@ -66,7 +89,7 @@
           let ball = this.balls[count];
           // 取到需要动的球,外层为纵向，内层为横向
           if (ball.show) {
-              // 外层ball是隐藏的，inner是展现的
+            // 外层ball是隐藏的，inner是展现的
             let rect = ball.el.getBoundingClientRect();
             let x = rect.left - 32;
             let y = -(window.innerHeight - rect.top - 22);
@@ -80,7 +103,7 @@
         }
       },
       enter (el) {
-          /* eslint-disable no-unused-vars */
+        /* eslint-disable no-unused-vars */
         let rf = el.offsetHeight;
         this.$nextTick(() => {
           el.style.display = '';
@@ -89,7 +112,7 @@
           let inner = el.getElementsByClassName('inner-hook')[0];
           inner.style.webkitTransform = `translate3d(0,0,0)`;
           inner.style.transform = `translate3d(0,0,0)`;
-          });
+        });
       },
       afterEnter (el) {
         let ball = this.dropBalls.shift();
@@ -108,17 +131,18 @@
           {show: false},
           {show: false}
         ],
-        dropBalls: []
+        dropBalls: [],
+        listShow: false
       };
     },
     computed: {
-        totlePrice () {
-          let total = 0;
-          this.selectFoods.forEach((food) => {
-              total += food.price * food.count;
-          });
-          return total;
-        },
+      totlePrice () {
+        let total = 0;
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count;
+        });
+        return total;
+      },
       totleCount () {
         let count = 0;
         this.selectFoods.forEach((food) => {
@@ -132,97 +156,138 @@
         } else if (this.totlePrice < this.minPrice) {
           return '还差￥' + (this.minPrice - this.totlePrice) + '元起送';
         } else {
-            return '去结算';
+          return '去结算';
         }
       }
+    },
+    components: {
+      cartControl
     }
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-.shopcart
-  position fixed
-  left 0
-  bottom 0
-  z-index 50
-  width 100%
-  background-color: #141d27
-  .ball-container
-    .ball
-      position fixed
-      left 16px
-      bottom 22px
-      z-index 200
-      transition all 0.5s cubic-bezier(0.49,-0.29,0.75,0.41)
-      .inner
-        width 16px
-        height 16px
-        border-radius 50%
-        background rgb(0,160,220)
-        transition all 0.5s linear
-  .content
-    display flex
-    background: #141d27
-    .content-left
-      flex 1
-      .logo
-        display inline-block
-        position relative
-        margin-top -10px
-        border-radius 50%
-        padding: 6px
-        box-sizing border-box
-        background-color #141d27
-        &.highlight
-          background-color rgb(0,160,220)
-        .number
-          position absolute
-          right 0px
-          top 0
-          width 24px
+  .shopcart
+    position fixed
+    left 0
+    bottom 0
+    z-index 50
+    width 100%
+    background-color: #141d27
+    .ball-container
+      .ball
+        position fixed
+        left 16px
+        bottom 22px
+        z-index 200
+        transition all 0.5s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+        .inner
+          width 16px
           height 16px
-          line-height 16px
+          border-radius 50%
+          background rgb(0, 160, 220)
+          transition all 0.5s linear
+    .shopcart-list
+      width 100%
+      position absolute
+      left 0
+      top -48px
+      z-index -1
+      color #fff
+      transition all 0.5s
+      transform translate3d(0,0,0)
+      .list-header
+        height: 40px
+        padding 0 18px
+        background: #f3f5f7
+        border-bottom 1px solid rgba(7,17,27,0.1)
+        .title
+          float left
+          line-height 40px
+          font-size 14px
+          color rgb(7,17,27)
+          margin 0
+        .empty
+          float right
+          font-size 12px
+          color: rgb(0,160,220)
+          line-height 40px
+        .list-content
+          padding 0 18px
+          max-height 217px
+          background: #fff
+          overflow hidden
+    .content
+      display flex
+      background: #141d27
+      .content-left
+        flex 1
+        .logo
+          display inline-block
+          position relative
+          margin-top -10px
+          border-radius 50%
+          padding: 6px
+          box-sizing border-box
+          background-color #141d27
+          &.highlight
+            background-color rgb(0, 160, 220)
+          .number
+            position absolute
+            right 0px
+            top 0
+            width 24px
+            height 16px
+            line-height 16px
+            text-align center
+            border-radius 16px
+            font-size 9px
+            font-weight 700px
+            color #ffffff
+            background-color #f00
+            box-shadow 0px 4px 8px 0px rgba(0, 0, 0, 0.4)
+        .price
+          display inline-block
+          vertical-align top
+          line-height 24px
+          font-size 16px
+          margin-top 12px
+          padding-right 12px
+          border-right 1px solid rgba(255, 255, 255, 0.1)
+          font-weight 700
+          color rgba(255, 255, 255, 0.4)
+        .describe
+          display inline-block
+          vertical-align top
+          margin 12px 0 0 0
+          line-height 24px
+          padding-left 12px
+          font-weight 700
+          font-size 10px
+          color rgba(255, 255, 255, 0.4)
+      .content-right
+        flex 0 0 100px
+        width 100px
+        .pay
+          height 48px
+          line-height: 48px
+          font-size 12px
           text-align center
-          border-radius 16px
-          font-size 9px
-          font-weight 700px
-          color #ffffff
-          background-color #f00
-          box-shadow 0px 4px 8px 0px rgba(0,0,0,0.4)
-      .price
-        display inline-block
-        vertical-align top
-        line-height 24px
-        font-size 16px
-        margin-top 12px
-        padding-right 12px
-        border-right 1px solid rgba(255,255,255,0.1)
-        font-weight 700
-        color rgba(255,255,255,0.4)
-      .describe
-        display inline-block
-        vertical-align top
-        margin 12px 0 0 0
-        line-height 24px
-        padding-left 12px
-        font-weight 700
-        font-size 10px
-        color rgba(255,255,255,0.4)
-    .content-right
-      flex 0 0 100px
-      width 100px
-      .pay
-        height 48px
-        line-height: 48px
-        font-size 12px
-        text-align center
-        font-weight 700
-        color rgba(255,255,255,0.4)
-        background-color #2b333b
-        &.highlight
-          background-color rgb(0,160,220)
-          color #fff
-        &.enough
-          background-color #42b983
+          font-weight 700
+          color rgba(255, 255, 255, 0.4)
+          background-color #2b333b
+          &.highlight
+            background-color rgb(0, 160, 220)
+            color #fff
+          &.enough
+            background-color #42b983
 
+</style>
+<style>
+  .cartFold-enter-active, .cartFold-leave-active {
+    opacity:1;
+  }
+  .cartFold-enter, .cartFold-leave-active {
+    opacity:0;
+  }
 </style>
